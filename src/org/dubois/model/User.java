3a57917c21e3fecb2f5eda89f886a039cb483335
Sébastien DUBOIS <snx.dubois@gmail.com>
2020-03-30 12:08:43 +0200

    added getter and setter

--- src/org/dubois/model/User.java
+++ src/org/dubois/model/User.java
@@ -4,6 +4,11 @@ public class User {
 
     private String name;
 
-    
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }    
 }
